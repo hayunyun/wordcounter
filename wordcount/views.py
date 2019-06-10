6b07e00b800f7eb5f2fd66b11ed0dd35b2962066
Yunyoung Ha <hayunyun@naver.com>
2019-06-10 10:14:34 +0000

    first and last

---
+++ wordcount/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+    
+def about(request):
+    return render(request, 'about.html')
+    
+def result(request):
+    text = request.GET['fulltext']
+    words = text.split(" ")
+    word_dictionary = {}
+    
+    for word in words:
+        if word in word_dictionary:
+            #1증가
+            word_dictionary[word]+=1 
+        else:
+            #새롭게 1 생성
+            word_dictionary[word]=1 
+    
+        
+    return render(request, 'result.html', {'full' : text, 'total_t' : len(text),'total_w' : len(words), 'dictionary' : word_dictionary.items()})
